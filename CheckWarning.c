156034951154793e156218d7d0140a0cf5e469ec
Preethiravi04 <preethiravi0499@gmail.com>
2024-08-11 21:15:30 +0530

    Create CheckWarning.c

---
+++ CheckWarning.c
@@ -0,0 +1,28 @@
+#include "CheckWarning.h"
+
+static char WarnMessage[50];
+
+static int IsWarningFound(float value ,ParameterBoundaries boundaries, int NoOfBoundaryInputs);
+
+static int IsWarningFound(float value ,ParameterBoundaries boundaries, int NoOfBoundaryInputs)
+{
+    int retVal = 0;
+    for (int i = 0; i < NoOfBoundaryInputs; ++i) {
+        if (IsInputWithinTheRange(value, boundaries.Boundary[i].lowerLimit, boundaries.Boundary[i].upperLimit)) {
+            strcpy(WarnMessage, boundaries.Boundary[i].warnMessage);
+            retVal = 1;
+        }
+    }
+    return retVal;
+}
+
+void checkForWarnings(float value ,ParameterBoundaries boundaries, int NoOfBoundaryInputs, bool performWarnCheck)
+{
+  if(performWarnCheck)
+  {
+      if(IsWarningFound(value, boundaries, NoOfBoundaryInputs))
+      {
+        printOnConsole(WarnMessage);
+      }
+  }
+}
